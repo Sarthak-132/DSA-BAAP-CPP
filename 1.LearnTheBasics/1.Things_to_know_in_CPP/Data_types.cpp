80b8dcc94b7dcc215137cbe769e9dcf7ac93126d
Sarthak dhoke <dksarthak101@gmail.com>
2025-04-17 12:18:54 +0530

    cpp started with user i/o and data types

---
+++ 1.LearnTheBasics/1.Things_to_know_in_CPP/Data_types.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+	int main() {
+		float x = 3.1;
+		float y = 4;
+		double z = 5;
+		string s = "Hello Buddy";
+		string t, t2;
+		cin >> t >> t2;
+		string str;
+		getline(cin, str);
+
+		cout << x << " " << " " << y << " " << z << endl;
+		cout << s << endl;
+		cout << t << " " << t2 << endl;
+		cout << str << endl;
+
+		return 0;
+
+	}
