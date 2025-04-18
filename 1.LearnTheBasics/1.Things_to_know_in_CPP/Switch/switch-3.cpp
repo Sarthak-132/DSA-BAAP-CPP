5f23d236948769ff9e0ccbd9011c4946af592380
Sarthak dhoke <dksarthak101@gmail.com>
2025-04-18 11:31:13 +0530

    done the topics till switch statements

---
+++ 1.LearnTheBasics/1.Things_to_know_in_CPP/Switch/switch-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'B';
+
+    switch (grade) {
+        case 'A':
+            cout << "Excellent!" << endl;
+            break;
+        case 'B':
+            cout << "Good!" << endl;
+            break;
+        default:
+            cout << "Not specified." << endl;
+    }
+
+    return 0;
+}
