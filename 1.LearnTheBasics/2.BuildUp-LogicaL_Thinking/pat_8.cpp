13db640881c5f853284e5cd56ff952e8ba7a3a8a
Sarthak dhoke <dksarthak101@gmail.com>
2025-04-23 11:35:29 +0530

    done pattern - 8

--- 1.LearnTheBasics/2.BuildUp-LogicaL_Thinking/pat_8.cpp
+++ 1.LearnTheBasics/2.BuildUp-LogicaL_Thinking/pat_8.cpp
@@ -9,13 +9,13 @@ using namespace std;
 void pattern8(int N){
 
     for(int i = 0 ; i < N ; i++){
-        for(int j = 0 ; j < (N-i-1); j++){
+        for(int j = 0 ; j < i; j++){
             cout << " ";
         }
-        for(int j = 0 ; j < (2*i+1); j++){
+        for(int j = 0 ; j < 2*N-(2*i + 1); j++){
             cout << "*";
         }
-        for(int j = 0 ; j < (N-i-1); j++){
+        for(int j = 0 ; j < i; j++){
             cout << " ";
         }
         cout << endl;
